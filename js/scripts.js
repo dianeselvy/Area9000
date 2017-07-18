2db9d98cd2de1c1177a0ee21b84de738c23f2102
Diane Selvy <dianeselvy@hotmail.com>
2017-07-18 11:45:35 -0700

    Includes splash page with "ENTER" fadeout effect

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//$('.enter_link').click(function() {
+//    $(this).parent('#splashscreen').fadeOut(500);
+//});
+
+
+/*
+$( "#splash" ).click(function() {
+  $( "#splashufo" ).fadeOut( "slow", function() {
+    // Animation complete.
+  });
+});
+*/
